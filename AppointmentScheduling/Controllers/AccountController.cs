0008cba0ae0f10940a43f379164835871cb3fe16
Piotr Bąk <piotr.andrzej.bak@outlook.com>
2022-05-28 01:01:49 +0200

    r67

--- AppointmentScheduling/Controllers/AccountController.cs
+++ AppointmentScheduling/Controllers/AccountController.cs
@@ -12,7 +12,7 @@ namespace AppointmentScheduling.Controllers
             _db = db;
         }
 
-        public IActionResult Index()
+        public IActionResult Login()
         {
             return View();
         }
